8533f16e61e3c9de69fcaeef0d027f1f2dac7403
Dany-Rolon <dany_rolon@outlook.com>
2021-08-31 09:45:23 -0300

    Update Users.tsx
    Se añadio el tipado del resultado de la promesa

--- src/api/Users.tsx
+++ src/api/Users.tsx
@@ -1,6 +1,7 @@
 import {url} from './config';
+import { Result } from '../interfaces/contactsInterface';
 
-export function getUsers(): Promise<any> {
+export function getUsers(): Promise<Result> {
   return fetch(`${url}/?results=20`)
     .then(res => res.json())
     .then(data => data)
